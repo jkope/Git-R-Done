4aa54a29b2e4d31f7596a698bb48f69f1556c47e
Jake K <jake.kopenhefer@gmail.com>
2018-09-27 19:46:53 -0600

    add delete function to task

--- js/index.js
+++ js/index.js
@@ -9,16 +9,30 @@ let main = '';
 
 function tabs(listNames) {
     hTabs ='';
+    main = '';
     for (i=0; i<listNames.length; i++){
         hTabs +=
         `<a href="#scroll-tab-${i}" class="mdl-layout__tab">${listNames[i]}</a></br>`;
-        
+        main +=
+        `<section class="mdl-layout__tab-panel" id="scroll-tab-${i}">
+                <div class="page-content">
+                    <!-- Your content goes here -->
+                   <p> stuff test stuff </p>
+                </div>
+            </section>`
     }
     headerTabs.innerHTML = hTabs;
+    pages.innerHTML = main;
 }
 
 listBtn.addEventListener('click', function(){
     listNames.push(listNameInput.value);
     listNameInput.value = '';
     tabs(listNames);
+    // document.getElementsByName('listNames["length"]').focus();
 });
+
+function deleteTask(task) {
+    let line = document.getElementById(task);
+    line.parentNode.removeChild(line);
+}
