214637f23f241e77b89f584d6570448b1dfc91b3
Jake K <jake.kopenhefer@gmail.com>
2018-09-30 21:17:38 -0600

    delete list repopulates the next list down

--- js/index.js
+++ js/index.js
@@ -16,9 +16,9 @@ function tabs(listNames) {
     main = '';
     for (i=0; i<listNames.length; i++){
         hTabs +=
-            `<a id='link-${i}' href="#scroll-tab-${i}" class="mdl-layout__tab ${i == (listNames.length - 1) ? 'is-active' : ''}">${listNames[i]}</a>`;
+            `<a id='link-${i}' href="#scroll-tab-${i}" class="mdl-layout__tab ${i == (listNames.length - 1) ? 'is-active' : ''}">${listNames[i]}</a>`
         main +=
-        `<section class="mdl-layout__tab-panel  ${i == (listNames.length-1) ? 'is-active':''}" id ="scroll-tab-${i}">
+            `<section class="mdl-layout__tab-panel  ${i == (listNames.length - 1) ? 'is-active' : ''}" id ="scroll-tab-${i}">
             <div class="page-content">
                 <div id="list" class="list">
                 <!-- New Task Text field -->
@@ -31,11 +31,9 @@ function tabs(listNames) {
                         <i class="material-icons">add</i>
                     </button>
                 </form>
-                <!-- list of tasks -->
                 <div id="${i}">
                 ${i}
                 </div>
-                <!-- delete whole tab -->
                 <button onclick="deleteList('${i}')" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--red">
                     Delete List
                 </button>
@@ -67,20 +65,17 @@ function re_tabs(listNames) {
                         <i class="material-icons">add</i>
                     </button>
                 </form>
-                <!-- list of tasks -->
                 <div id="${i}">
                 ${i}
                 </div>
-                <!-- delete whole tab -->
                 <button onclick="deleteList('${i}')" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--red">
                     Delete List
                 </button>
             </div>
-        </section>`;
+        </section>`
     }
     headerTabs.innerHTML = hTabs;
     pages.innerHTML = main;
-    // taskList.push([]);
 }
 
 listBtn.addEventListener('click', function(){
@@ -98,12 +93,7 @@ function deleteList(i){
     listNames.splice(i , 1);
     taskList.splice(i , 1);
     re_tabs(listNames);
-
-
-    // let list = document.getElementById(scrolltab);
-    // list.parentNode.removeChild(list);
-    // let aref = document.getElementById(link);
-    // aref.parentNode.removeChild(aref)
+    i>0 ?  re_Task(i-1) : '';
 }
 
 function addTask(listnum) {
@@ -115,6 +105,26 @@ function addTask(listnum) {
     for (let i = 0; i < taskList[listnum].length; i++) {
         taskListHtml +=
        `<div id="task${listnum}_${i}" class="task">
+            <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox${listnum}_${i}">
+                <input type="checkbox" id="checkbox${listnum}_${i}" class="mdl-checkbox__input">
+                <span id="item${listnum}_${i}" class="mdl-checkbox__label">${taskList[listnum][i]}</span>
+            </label>
+            <i id='dlt${listnum}_${i}' onclick="deleteTask('task${listnum}_${i}', '${listnum}','${i}')" class="material-icons icon">
+                delete
+            </i>
+        </div>`
+    }
+    document.getElementById(listnum).innerHTML = taskListHtml;
+}
+function re_Task(listnum) {
+    let newTaskId = 'newTask' + listnum;
+    let input = document.getElementById(newTaskId).value;
+    // (taskList[listnum]).push(input);
+    document.getElementById(newTaskId).value = '';
+    taskListHtml = '';
+    for (let i = 0; i < taskList[listnum].length; i++) {
+        taskListHtml +=
+            `<div id="task${listnum}_${i}" class="task">
             <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox${listnum}_${i}">
                 <input type="checkbox" id="checkbox${listnum}_${i}" class="mdl-checkbox__input">
                 <span id="item${listnum}_${i}" class="mdl-checkbox__label">${taskList[listnum][i]}</span>
